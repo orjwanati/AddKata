e61aa58884cf6fd5edc27718333a77ae666313b7
orjuwan <orjwanati@hotmail.com>
2022-02-05 09:38:36 +0400

    add First Test

--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,3 @@
 module addKata {
+	requires org.junit.jupiter.api;
 }
--- src/test/AddKataTest.java
+++ src/test/AddKataTest.java
@@ -1,10 +1,16 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.fail;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class AddKataTest {
+
+	@BeforeAll
+	void setUp() throws Exception {
+	
+	}
 
 	@Test
 	void test() {
